f5990ccb106d609048465ea0cb0b05dfedec1ca2
Jamie <jamie@Benjis-MacBook-Pro.local>
2020-04-14 16:33:41 -0600

    router and paths

--- src/components/sideNav/SideNav.tsx
+++ src/components/sideNav/SideNav.tsx
@@ -1,47 +1,61 @@
 import * as React from 'react';
-import {useState} from "react";
+import {useEffect, useState} from 'react';
+import {
+    BrowserRouter as Router,
+    Link
+} from "react-router-dom";
 
 const styles = require('./SideNav.module.scss');
 
-export const navLinks : {id: number, text: string}[] = [
+export const navLinks : {id: number, text: string, path: string}[] = [
     {
         id: 1,
-        text: 'Daily Bulletin'
+        text: 'Daily Bulletin',
+        path: '/bulletin'
     },
     {
         id: 2,
-        text: 'Watch Lesson'
+        text: 'Watch Lesson',
+        path: '/watch'
     },
     {
         id: 3,
-        text: 'Q&A Board'
+        text: 'Q&A Board',
+        path: '/q-and-a'
     },
     {
         id: 4,
-        text: 'Grades Dashboard'
+        text: 'Grades Dashboard',
+        path: '/grades'
     }
 ]
 
 function SideNav() {
-    const [activeTab, setActiveTab] = useState(1);
+    const [activeTab, setActiveTab] = useState(navLinks[0].path);
 
-    function handleTabClick(id: number) {
-        setActiveTab(id);
+    useEffect(() => {
+        const currentUrlPath = window.location.pathname
+        if (currentUrlPath !== activeTab) {
+            setActiveTab(currentUrlPath)
+        }
+    })
 
-        return id;
+    function handleTabClick(path: string) {
+        setActiveTab(path);
     }
 
     return (
-        <ul className={styles.wrapper}>
-            {navLinks.map(navItem => <li
-                key={navItem.id}
-                className={activeTab === navItem.id ? `${styles.listItem} ${styles.active}` : styles.listItem}
-                onClick={() => handleTabClick(navItem.id)}
-                data-testid="tab"
-            >
-                {navItem.text}
-            </li>)}
-        </ul>
+        <Router>
+            <ul className={styles.wrapper}>
+                {navLinks.map(navItem => <Link to={navItem.path} key={navItem.id}><li
+                    className={activeTab === navItem.path ? `${styles.listItem} ${styles.active}` : styles.listItem}
+                    onClick={() => handleTabClick(navItem.path)}
+                    data-testid="tab"
+                >
+                    {navItem.text}
+                </li></Link>)}
+            </ul>
+        </Router>
     );
 }
 
